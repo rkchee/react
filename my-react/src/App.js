cb96e4bc871ceca986430c936461b2909ddc74e6
Roger Ng, MD <rkchee@gmail.com>
2018-11-24 09:57:43 -0800

    added my name

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Roger's new reactor
           </a>
         </header>
       </div>
