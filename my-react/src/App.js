bcd756176913ccb8cc659d7e663a9eeee3223b8e
Roger Ng, MD <rkchee@gmail.com>
2018-11-25 08:22:15 -0800

    added ChipsArray

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -22,6 +22,7 @@ class App extends Component {
           </a>
           <SimpleSnackbar></SimpleSnackbar>
           <SpeedDials></SpeedDials>
+          <ChipsArray></ChipsArray>
         </header>
       </div>
     );
