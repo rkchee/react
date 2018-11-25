a9652eb1a654d14dc76000fc3a80cb91840f65a5
Roger Ng, MD <rkchee@gmail.com>
2018-11-25 11:33:13 -0800

    Color

--- my-react/src/ChipsArray.js
+++ my-react/src/ChipsArray.js
@@ -12,7 +12,7 @@ const styles = theme => ({
     flexWrap: 'wrap',
     padding: theme.spacing.unit / 2,
     background: '#282c34',
-    flexDirection: 'column',
+    flexFlow: 'column wrap-reverse',
     alignItems: 'end',
   },
   chip: {
