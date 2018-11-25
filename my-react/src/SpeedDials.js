2e19a246731213add2eebfe694f7c1d483246964
Roger Ng, MD <rkchee@gmail.com>
2018-11-24 21:47:27 -0800

    fixed 60 min

--- my-react/src/SpeedDials.js
+++ my-react/src/SpeedDials.js
@@ -53,7 +53,7 @@ const actions = [
   { icon: <SaveIcon />, name: 'Save' },
   { icon: <PrintIcon />, name: 'Print' },
   { icon: <ShareIcon />, name: 'Share' },
-  { icon: <p>"60min"</p>, name: 'Delete' },
+  { icon: "60min", name: 'Delete' },
 ];
 
 class SpeedDials extends React.Component {
