77058e2086ef18ed00dc929a1f0ced42a5289ef2
Roger Ng, MD <rkchee@gmail.com>
2018-11-25 10:41:53 -0800

    Color

--- my-react/src/ChipsArray.js
+++ my-react/src/ChipsArray.js
@@ -8,7 +8,7 @@ import TagFacesIcon from '@material-ui/icons/TagFaces';
 const styles = theme => ({
   root: {
     display: 'flex',
-    justifyContent: 'left',
+    justifyContent: 'center',
     flexWrap: 'wrap',
     padding: theme.spacing.unit / 2,
     background: '#282c34',
